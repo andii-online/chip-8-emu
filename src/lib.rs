dbb22db95028752b9a7936c133f48491f035ef81
andrewstraus99 <andrewstraus99@gmail.com>
2022-06-21 11:04:55 -0500

    Update palettes?

--- src/lib.rs
+++ src/lib.rs
@@ -33,31 +33,31 @@ pub struct Palette {
 const DEFAULT_PALETTE: Palette = Palette {
     background: Color::RGB(34, 35, 35),
     foreground: Color::RGB(240, 246, 240),
-    gutter: Color::BLACK,
+    gutter: Color::RGB(255 - 34, 255 - 35, 255 - 35),
 };
 
 const BITBEE: Palette = Palette {
     background: Color::RGB(41, 43, 48),
     foreground: Color::RGB(207, 171, 74),
-    gutter: Color::BLACK,
+    gutter: Color::RGB(255 - 41, 255 - 43, 255 - 48),
 };
 
 const NEUTRAL_GREEN: Palette = Palette {
     background: Color::RGB(0, 76, 61),
     foreground: Color::RGB(255, 234, 249),
-    gutter: Color::BLACK,
+    gutter: Color::RGB(255, 255 - 76, 255 - 61),
 };
 
 const MAC_PAINT: Palette = Palette {
     background: Color::RGB(139, 200, 254),
     foreground: Color::RGB(5, 27, 44),
-    gutter: Color::BLACK,
+    gutter: Color::RGB(255 - 139, 255 - 200, 255 - 254),
 };
 
 const PAPER_BACK: Palette = Palette {
     background: Color::RGB(184, 194, 185),
     foreground: Color::RGB(56, 43, 38),
-    gutter: Color::BLACK,
+    gutter: Color::RGB(255 - 184, 255 - 194, 255 - 185),
 };
 
 pub const PALETTES: [Palette; 5] = [
